7f0bf47dd71cee15864c9f08cde1c2e8a247aff4
Norbert <bnorbert88@gmail.com>
2019-09-21 17:42:19 +0200

    Different objects

--- render.py
+++ render.py
@@ -28,10 +28,40 @@ def absoluteLowestPoint():
         return minv
 
 
+def randomObject():
+    a=random.random()
+    if a < 0.33:
+        return randomCube()
+    if a < 0.66:
+        return randomTorus()
+    return randomMonkey()
+
 
 def randomCube():
     bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0),
-                                    rotation=(random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)))
+                                    rotation=(
+                                    random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)))
+    c = bpy.context.object
+    mat = bpy.data.materials.new("Mat1")
+    mat.diffuse_color = (random.random(), random.random(), random.random())
+    c.active_material = mat
+    return c
+
+
+def randomMonkey():
+    bpy.ops.mesh.primitive_monkey_add(location=(0, 0, 0),
+                                      rotation=(0, 0, random.uniform(0, 3.14)))
+    c = bpy.context.object
+    mat = bpy.data.materials.new("Mat1")
+    mat.diffuse_color = (random.random(), random.random(), random.random())
+    c.active_material = mat
+    return c
+
+
+def randomTorus():
+    bpy.ops.mesh.primitive_torus_add(location=(0, 0, 0),
+                                     rotation=(
+                                     random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)))
     c = bpy.context.object
     mat = bpy.data.materials.new("Mat1")
     mat.diffuse_color = (random.random(), random.random(), random.random())
@@ -53,11 +83,12 @@ world.light_settings.environment_energy = 0.2
 
 # Add a camera
 bpy.ops.object.camera_add(location=(6, -3, 5), rotation=(0, 0, 0))
+bpy.context.object.name = "camera1"
 bpy.context.object.rotation_euler[0] = 0.9
 bpy.context.object.rotation_euler[1] = 0.0
 bpy.context.object.rotation_euler[2] = 1.1
 
-for x in range(1):
+for x in range(10):
     # Delete the original default objects
     bpy.ops.object.select_by_type(type='MESH')
     bpy.ops.object.delete(use_global=False)
@@ -68,7 +99,7 @@ for x in range(1):
     randomLamp("lamp1")
 
     # Create a cube
-    cube = randomCube()
+    cube = randomObject()
 
     # Create the plane
     bpy.ops.mesh.primitive_plane_add(radius=10, location=absoluteLowestPoint())
@@ -84,6 +115,3 @@ for x in range(1):
     bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.context.scene.render.filepath = "D:/kocka/images/image" + str(x) + "noshadow.png"
     bpy.ops.render.render(write_still=1)
-
-
-
