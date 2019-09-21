e95807a09e1dd1f232a34b1577c6b1c88b0cb60f
Norbert <bnorbert88@gmail.com>
2019-09-21 15:57:18 +0200

    Functions to automatize, updated gitignore

--- render.py
+++ render.py
@@ -1,20 +1,55 @@
 import bpy
 import random
 
+
 def randomLightPosition():
     x = random.uniform(2, 4) * pow(-1, random.randint(1, 2))
     y = random.uniform(2, 4) * pow(-1, random.randint(1, 2))
     z = random.uniform(0.2, 5)
     return (x, y, z)
 
-# Delete the original objects
-bpy.ops.object.select_by_type(type='MESH')
-bpy.ops.object.delete(use_global=False)
-bpy.ops.object.select_by_type(type='LAMP')
-bpy.ops.object.delete(use_global=False)
 
-bpy.data.worlds['World'].light_settings.use_environment_light = True
-bpy.data.worlds['World'].light_settings.environment_energy = 0.2
+def objectLowestPoint(obj):
+    matrix_w = obj.matrix_world
+    vectors = [matrix_w * vertex.co for vertex in obj.data.vertices]
+    return min(vectors, key=lambda item: item.z)
+
+
+def absoluteLowestPoint():
+    bpy.ops.object.select_all(action='SELECT')
+    obs = bpy.context.selected_objects
+    minv = [0, 0, 0]
+    for obj in obs:
+        matrix_w = obj.matrix_world
+        vectors = [matrix_w * vertex.co for vertex in obj.data.vertices]
+        v = min(vectors, key=lambda item: item.z)
+        if minv[2] > v[2]:
+            minv = v
+        return minv
+
+
+
+def randomCube():
+    bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0),
+                                    rotation=(random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)))
+    c = bpy.context.object
+    mat = bpy.data.materials.new("Mat1")
+    mat.diffuse_color = (random.random(), random.random(), random.random())
+    c.active_material = mat
+    return c
+
+
+def randomLamp(name):
+    pos = randomLightPosition()
+    bpy.ops.object.lamp_add(type='POINT', location=pos)
+    bpy.context.object.data.shadow_method = 'RAY_SHADOW'
+    bpy.context.object.name = name
+
+
+# Setup a global environmental lightning
+world = bpy.data.worlds['World']
+world.light_settings.use_environment_light = True
+world.light_settings.environment_energy = 0.2
 
 # Add a camera
 bpy.ops.object.camera_add(location=(6, -3, 5), rotation=(0, 0, 0))
@@ -22,51 +57,33 @@ bpy.context.object.rotation_euler[0] = 0.9
 bpy.context.object.rotation_euler[1] = 0.0
 bpy.context.object.rotation_euler[2] = 1.1
 
-# Add a lamp.
-bpy.ops.object.lamp_add(type='POINT', location=(3, 2, 6))
-bpy.context.object.data.shadow_method = 'RAY_SHADOW'
-bpy.context.object.name = "Light"
-
-# Create a cube
-bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0))
-bpy.context.object.name = "Cube1"
-mat = bpy.data.materials.new("Mat1")
-mat.diffuse_color = (1.2, 3.0, 1.0)
-bpy.context.selected_objects[0].active_material = mat
-
-# Create the plane
-bpy.ops.mesh.primitive_plane_add(radius=10, location=(0, 0, -1))
-
-# render settings
-
-
-for x in range(3):
-    pos = randomLightPosition()
-
+for x in range(1):
+    # Delete the original default objects
+    bpy.ops.object.select_by_type(type='MESH')
+    bpy.ops.object.delete(use_global=False)
     bpy.ops.object.select_by_type(type='LAMP')
     bpy.ops.object.delete(use_global=False)
 
     # Add a lamp.
-    bpy.ops.object.lamp_add(type='POINT', location=pos)
-    bpy.context.object.data.shadow_method = 'RAY_SHADOW'
-    bpy.context.object.name = "Light"
+    randomLamp("lamp1")
 
+    # Create a cube
+    cube = randomCube()
+
+    # Create the plane
+    bpy.ops.mesh.primitive_plane_add(radius=10, location=absoluteLowestPoint())
+
+    # Rendering
     bpy.context.scene.render.image_settings.file_format = 'PNG'
-    bpy.context.scene.render.filepath = "D:/kocka/image" + str(x) + "shadow.png"
+    bpy.context.scene.render.filepath = "D:/kocka/images/image" + str(x) + "shadow.png"
     bpy.ops.render.render(write_still=1)
 
-    bpy.ops.object.select_by_type(type='LAMP')
-    bpy.ops.object.delete(use_global=False)
+    bpy.data.objects['lamp1'].data.shadow_method = 'NOSHADOW'
 
-    # Add a lamp.
-    bpy.ops.object.lamp_add(type='POINT', location=pos)
-    bpy.context.object.data.shadow_method = 'NOSHADOW'
-    bpy.context.object.name = "Light"
-
+    # Rendering
     bpy.context.scene.render.image_settings.file_format = 'PNG'
-    bpy.context.scene.render.filepath = "D:/kocka/image" + str(x) + "no_sh.png"
+    bpy.context.scene.render.filepath = "D:/kocka/images/image" + str(x) + "noshadow.png"
     bpy.ops.render.render(write_still=1)
-
 
 
 
