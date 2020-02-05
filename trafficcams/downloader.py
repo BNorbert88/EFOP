2200b1811179df8973fd1c8053e8a3428328b705
Miklos Maroti <mmaroti@gmail.com>
2020-02-05 19:49:18 +0100

    better error handling, detect partially downloaded images

--- trafficcams/downloader.py
+++ trafficcams/downloader.py
@@ -3,10 +3,14 @@
 # Copyright (C) 2019-2020, University of Szeged, Bogya Norbert
 #
 
+from datetime import datetime
 import json
 import os
-from datetime import datetime
+from io import BytesIO
+import time
+import numpy as np
 import requests
+from PIL import Image
 
 FOLDER = 'images/'
 APIKEY = 'SgdO57pmM7byDpbENqVkscYwdiF0G0GSsFwA'  # change it for yours
@@ -16,38 +20,63 @@ def save_all_traffic_cam():
     if not os.path.isdir(FOLDER):
         os.mkdir(FOLDER)
 
-    response = requests.get('https://api.transport.nsw.gov.au/v1/live/cameras',
-                            headers={'Authorization': 'apikey ' + APIKEY})
-    features = json.loads(response.content)['features']
-    print("active cameras", len(features))
-
-    for feature in features:
-        cam_id = feature['id']
-        print("downloading from", cam_id)
-        url = feature['properties']['href']
+    features = []
+    while True:
         try:
-            response2 = requests.get(url, stream=False)
-            if response2.status_code != 200:
-                print("unexpected status code", response2.status_code)
+            response = requests.get('https://api.transport.nsw.gov.au/v1/live/cameras',
+                                    headers={'Authorization': 'apikey ' + APIKEY})
+            features = json.loads(response.content)['features']
+            print("number of active cameras:", len(features))
+        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError):
+            print("failed to update list of cameras")
+            if len(features) < 10:
+                time.sleep(60)
+
+        for feature in features:
+            cam_id = feature['id']
+            print("downloading image from:", cam_id)
+            url = feature['properties']['href']
+            try:
+                response = requests.get(url, stream=False)
+                if response.status_code != 200:
+                    print("unexpected response:", response.status_code)
+                    continue
+
+                img = Image.open(BytesIO(response.content))
+                if np.array(img).shape == ():
+                    print("broken image from:", cam_id)
+                    continue
+
+                folder_name = os.path.join(FOLDER, cam_id)
+                if not os.path.isdir(folder_name):
+                    os.mkdir(folder_name)
+                file_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')
+                file_name = os.path.join(folder_name, cam_id + '-' + file_time + '.jpg')
+                with open(file_name, 'wb') as f:
+                    f.write(response.content)
+
+                feature['file_name'] = file_name
+                feature['file_time'] = file_time
+                with open(os.path.join(FOLDER, 'adatok.txt'), 'at') as f:
+                    f.write(json.dumps(feature) + '\n')
+
+            except requests.exceptions.ConnectionError:
+                print("download failed from:", cam_id)
+
+
+def delete_broken_images():
+    for dir_name, _, file_names in os.walk(FOLDER):
+        for file_name in file_names:
+            if not file_name.endswith('.jpg'):
                 continue
 
-            folder_name = os.path.join(FOLDER, cam_id)
-            if not os.path.isdir(folder_name):
-                os.mkdir(folder_name)
-            file_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')
-            file_name = os.path.join(folder_name, cam_id + '-' + file_time + '.jpg')
-            with open(file_name, 'wb') as f:
-                f.write(response2.content)
-
-            feature['file_name'] = file_name
-            feature['file_time'] = file_time
-            with open(os.path.join(FOLDER, 'adatok.txt'), 'at') as f:
-                f.write(json.dumps(feature) + '\n')
-
-        except requests.exceptions.ConnectionError:
-            print("download failed from", cam_id)
+            file_name = os.path.join(dir_name, file_name)
+            img = np.array(Image.open(file_name))
+            if img.shape == ():
+                print("deleting:", file_name)
+                os.unlink(file_name)
 
 
 if __name__ == '__main__':
-    while True:
-        save_all_traffic_cam()
+    save_all_traffic_cam()
+    # delete_broken_images()
